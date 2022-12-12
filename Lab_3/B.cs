4c0dcf1902ede9f2c6afb3c6b8bb3161d123aad5
Belous Vladislav <114071220+BelousWladislaw@users.noreply.github.com>
2022-12-12 15:22:14 +0300

    Add files via upload

---
+++ Lab_3/B.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belous_labs
+{
+    internal class B : A
+    {
+        public int d;
+
+        public int[] arr;
+        public B(int d)
+        {
+            a = 7;
+            b = 1;
+            this.d = d;
+        }
+        public B() : this(3)
+        {
+            arr = new int[a];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = C2 * i;
+
+        }
+        public B(int a, int b, int d) : base(a, b)
+        {
+
+            this.d = d;
+        }    
+        public float C3
+        {
+            get
+            {
+                for (int i = 0; i < d; i++)
+                {
+                    a *= b;
+                }
+                return a;
+            }
+        }
+    }
+}
+
+
---
+++ Lab_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using Belous_labs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belous_labs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {                  
+            B b = new B();
+            foreach (int el in b.arr)
+            {
+                Console.WriteLine(el);
+            }
+                     
+        }
+    }
+}
