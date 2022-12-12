6055e8daa87ae9769fae43f1961157d7bd5bddb3
Belous Vladislav <114071220+BelousWladislaw@users.noreply.github.com>
2022-12-12 15:19:22 +0300

    Add files via upload

---
+++ Lab_1/A.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belous_labs
+{
+    internal class A
+    {
+        private int a;
+
+        private int b;
+
+        public A() { }
+
+        public A(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public float C1()
+        {
+            return a * b;
+        }
+
+        public float C2()
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Lab_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Belous_labs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belous_labs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            A a = new A(3, 5);
+            Console.WriteLine(a.C1());
+            Console.WriteLine(a.C2());
+
+        }
+    }
+}
