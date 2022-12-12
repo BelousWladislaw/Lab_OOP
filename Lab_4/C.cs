bb9ec34eb69213b63b814ea472c896762a1fa089
Belous Vladislav <114071220+BelousWladislaw@users.noreply.github.com>
2022-12-12 15:31:12 +0300

    Add files via upload

---
+++ Lab_4/C.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belous_labs
+{
+    public class C<T>
+    {
+        public static int a = 5;
+
+        private T[] imyArray = new T[5];
+        public T this[int ind1]
+        {
+            get { return imyArray[ind1]; }
+            set { imyArray[ind1] = value; }
+        }
+    }
+
+}
+
+
---
+++ Lab_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using Belous_labs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belous_labs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            B b = new B();
+            
+            for (float i = 0; i < 10; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+
+            C<int> c1 = new C<int>();
+            C<string> c2 = new C<string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(c1[i]);
+                Console.WriteLine(c2[i]);
+            }
+          Console.Read();
+
+        }
+    }
+}
